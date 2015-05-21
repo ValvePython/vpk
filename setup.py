1be5e81952d4a0842065a5fbff1c5d449fb5e8ac
Rossen Georgiev <zoorty@gmail.com>
2015-05-21 23:59:27 +0100

    add py26 to setup file [ci skip]

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Natural Language :: English',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.0',
